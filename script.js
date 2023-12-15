ce5762aed9102c279df223774e7f1dbf18d5708d
unknown <ttddcc119@naver.com>
2023-12-15 23:02:03 +0900

    js 공부

---
+++ script.js
@@ -0,0 +1,7 @@
+const string = 'hello world';
+const string2 = 'lllll';
+const search = 'l';
+const regexp = new RegExp(search, 'g');
+console.log(regexp);
+console.log(typeof regexp);
+console.log(typeof '/l/g');
