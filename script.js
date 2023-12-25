f38edfa0f17d67a8c0acccbcaed171bbd25f0836
unknown <ttddcc119@naver.com>
2023-12-25 23:36:54 +0900

    js 공부

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-// 다른 작업자가 만들어둔 클래스
-
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  introduce() {
-    Object.entries(this).forEach(([property, value]) => {
-      console.log(`${property} : ${value}`);
-    });
-  }
+function isOddes(arr) {
+  arr.forEach((num) => {
+    if (num % 2) return true;
+    return false;
+  });
 }
-
-class newPerson extends Person {
-  constructor(name, age, password) {
-    super(name, age),
-      (this[Symbol('password')] = password),
-      (this[Symbol('birthCalcul')] = () => {
-        const currentDate = new Date();
-        return currentDate.getFullYear() - this.age;
-      });
-  }
-}
-
-const tom = new newPerson('tom', 16, 1234);
-tom.introduce();
