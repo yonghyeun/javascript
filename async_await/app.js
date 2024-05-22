4be774cf3a6afd406b8f481d30bf2bb2dca135ac
unknown <ttddcc119@naver.com>
2024-05-22 18:11:44 +0900

    HDD 공부

--- async_await/app.js
+++ async_await/app.js
@@ -1,30 +1,40 @@
-function processData(data) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve(data * 2), 1000);
-  });
-}
+// function* generatorMaker() {
+//   // console.log(generator.next()); 실행
+//   console.log('첫번째 호출');
+//   const x = yield 1;
+//   // console.log(generator.next(100)); 실행
+//   console.log('두 번째 호출'); // generator.next(100) 로 인해 x 엔 100이 할당
+//   const y = yield x + 10;
+//   // console.log(generator.next(200)); 실행
+//   console.log(y);
+//   const z = yield y + 10; // generator.next(200) 로 y 엔 200이 할당
+//   console.log(z);
+//   console.log('세 번째 호출');
+// }
 
-function* SugarCoat() {
-  const result1 = yield processData(10);
-  console.log('첫 번째 처리 결과:', result1);
-  const result2 = yield processData(result1);
-  console.log('두 번째 처리 결과:', result2);
-  const result3 = yield processData(result2);
-  console.log('세 번째 처리 결과:', result3);
-  return result3;
-}
+// const generator = generatorMaker();
+// console.log(generator.next());
+// console.log(generator.next(100));
+// console.log(generator.next(200));
+// console.log(generator.next());
 
-function async(generatorFunc) {
-  const generator = generatorFunc();
-
-  function onReslove(PromiseResult) {
-    const result = generator.next(PromiseResult);
-    return result.done
-      ? result.value
-      : result.value.then((res) => onReslove(res));
+const promise = new Promise((res, rej) => {
+  let i = 0;
+  while (i < 30000) {
+    i += 1;
   }
-
-  return onReslove;
-}
-
-async(SugarCoat)();
+  console.log(i);
+  res(i);
+})
+  .then((i) => {
+    while (i < 30000) {
+      i += 1;
+    }
+    console.log(i);
+    return i;
+  })
+  .then((i) => {
+    i += 1;
+    console.log(i);
+  });
+console.log(promise);
--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-const delay = (ms) => {
-  return new Promise((res) => {
-    setTimeout(() => {
-      res(true);
-    }, ms);
-  });
-};
-const fetching = async () => {
-  const result = await Promise.all([delay(1000), delay(2000), delay(3000)]);
-  console.log(result);
-  return result;
+/* HDD 스케줄링 */
+
+const test = (algorithm) => {
+  const req = [99, 184, 36, 123, 15, 125, 66, 68];
+  const current = 54;
+  const totalDistance = algorithm(req, current);
+  console.log(`${algorithm.name}의 전체 이동 거리는 ${totalDistance}`);
 };
 
-console.log('페칭 시작!');
-fetching();
+/* First Come First Serve */
+const FCFS = (req, current) => {
+  let _current = current;
+  let distance = 0;
+  for (let idx = 0; idx < req.length; idx += 1) {
+    const target = req[idx];
+    distance += Math.abs(target - _current);
+    _current = target;
+  }
+  return distance;
+};
+
+/* Shortest Seek Time First */
+
+const SSTF = (req, current) => {
+  let _current = current;
+  let distance = 0;
+
+  while (req.length !== 0) {
+    let short = { idx: 0, dist: Infinity };
+    req.forEach((target, idx) => {
+      const dist = Math.abs(_current - target);
+      if (dist < short.dist) {
+        short = { idx, dist };
+      }
+    });
+    console.log(_current, req[short.idx]);
+    _current = req[short.idx];
+    distance += short.dist;
+    req.splice(short.idx, 1);
+  }
+
+  return distance;
+};
+
+test(FCFS);
+test(SSTF);
